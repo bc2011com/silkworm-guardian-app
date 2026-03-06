db80fa7f59668c4cbbeae8465c733d6f3dbb62d3
Anny Jar <92168509+bc2011com@users.noreply.github.com>
2026-03-06 21:13:45 +0700

    Change Supabase URL and anon key
    Updated Supabase URL and anon key for the project.

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // TODO: ใส่ค่าจริงของโปรเจกต์ Supabase อาจารย์
-const SUPABASE_URL = "https://dpgnqrxanxyxhibkfspx.supabase.co";   // <- URL โปรเจกต์
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwZ25xcnhhbnh5eGhpYmtmc3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMjU5NjksImV4cCI6MjA3ODYwMTk2OX0.CTWYu4ZxS_gW6G4r0zGNNj6gVkNE6jSWmcGoXc6GaxI";              // <- anon key จริงของอาจารย์
+const supabaseUrl = "https://dtvtjioijdusoluuyius.supabase.co"
+const supabaseKey = "sb_publishable_yCqK4_WA6kjQEQ9QkpRuEQ_d3KLueBQ"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
